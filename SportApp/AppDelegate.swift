f838447001fc2136929f714a828ad9bf6725a9cb
Max K <KhizhniakovMA@gmail.com>
2020-12-02 11:45:27 +0300

    Initial commit

---
+++ SportApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SportApp
+//
+//  Created by Max Khizhniakov on 02.12.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
